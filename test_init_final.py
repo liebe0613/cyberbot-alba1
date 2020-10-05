b0aa10b69358f456001840e635fd9e1cea320f16
liebe0613 <53281788+liebe0613@users.noreply.github.com>
2020-10-06 00:40:50 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1218,44 +1218,44 @@ class mainCog(commands.Cog):
 	async def menu_(self, ctx):
 		if ctx.message.channel.id == basicSetting[7]:
 			command_list = ''
-			command_list += ','.join(command[2]) + '\n'     #!설정확인
-			command_list += ','.join(command[3]) + '\n'     #!채널확인
-			command_list += ','.join(command[4]) + ' [채널명]\n'     #!채널이동
-			command_list += ','.join(command[5]) + ' ※ 관리자만 실행 가능\n'     #!소환
-			command_list += ','.join(command[6]) + '\n'     #!불러오기
-			command_list += ','.join(command[7]) + '\n'     #!초기화
-			command_list += ','.join(command[8]) + '\n'     #!명치
-			command_list += ','.join(command[9]) + '\n'     #!재시작
-			command_list += ','.join(command[10]) + '\n'     #!미예약
-			command_list += ','.join(command[11]) + ' [인원] [금액]\n'     #!분배
-			command_list += ','.join(command[12]) + ' [뽑을인원수] [아이디1] [아이디2]...\n'     #!사다리
-			command_list += ','.join(command[27]) + ' [아이디1] [아이디2]...(최대 12명)\n'     #!경주
-			command_list += ','.join(command[35]) + ' [판매금액] (거래소세금)\n'     #!수수료
-			command_list += ','.join(command[36]) + ' [거래소금액] [실거래금액] (거래소세금)\n'     #!페이백
-			command_list += ','.join(command[13]) + ' [아이디]\n'     #!정산
-			command_list += ','.join(command[14]) + ' 또는 ' + ','.join(command[14]) + ' 0000, 00:00\n'     #!보스일괄
-			command_list += ','.join(command[15]) + '\n'     #!q
-			command_list += ','.join(command[16]) + ' [할말]\n'     #!v
-			command_list += ','.join(command[17]) + '\n'     #!리젠
-			command_list += ','.join(command[18]) + '\n'     #!현재시간
-			command_list += ','.join(command[24]) + '\n'     #!킬초기화
-			command_list += ','.join(command[25]) + '\n'     #!킬횟수 확인
-			command_list += ','.join(command[25]) + ' [아이디]\n'     #!킬
-			command_list += ','.join(command[26]) + ' [아이디]\n'     #!킬삭제
-			command_list += ','.join(command[33]) + ' [아이디] 또는 ' + ','.join(command[33]) + ' [아이디] [횟수]\n'     #!킬차감
-			command_list += ','.join(command[29]) + '\n'     #!아이템 목록 초기화
-			command_list += ','.join(command[30]) + '\n'     #!아이템 목록 확인
-			command_list += ','.join(command[30]) + ' [아이템] 또는 ' + ','.join(command[30]) + ' [아이템] [개수]\n'     #!아이템 목록 입력
-			command_list += ','.join(command[31]) + ' [아이템]\n'     #!아이템 목록에서 삭제
-			command_list += ','.join(command[32]) + ' [아이템] 또는 ' + ','.join(command[32]) + ' [아이템] [개수]\n'     #!아이템 차감
-			command_list += ','.join(command[19]) + '\n'     #!공지
-			command_list += ','.join(command[19]) + ' [공지내용]\n'     #!공지
-			command_list += ','.join(command[20]) + '\n'     #!공지삭제
-			command_list += ','.join(command[21]) + ' [할말]\n'     #!상태
-			command_list += ','.join(command[28]) + ' 사다리, 정산, 척살, 경주, 아이템\n'     #!채널설정
+			command_list += ','.join(command[2]) + '\n'     #/설정확인
+			command_list += ','.join(command[3]) + '\n'     #/채널확인
+			command_list += ','.join(command[4]) + ' [채널명]\n'     #/채널이동
+			command_list += ','.join(command[5]) + ' ※ 관리자만 실행 가능\n'     #/소환
+			command_list += ','.join(command[6]) + '\n'     #/불러오기
+			command_list += ','.join(command[7]) + '\n'     #/초기화
+			command_list += ','.join(command[8]) + '\n'     #/명치
+			command_list += ','.join(command[9]) + '\n'     #/재시작
+			command_list += ','.join(command[10]) + '\n'     #/미예약
+			command_list += ','.join(command[11]) + ' [인원] [금액]\n'     #/분배
+			command_list += ','.join(command[12]) + ' [뽑을인원수] [아이디1] [아이디2]...\n'     #/뽑기
+			command_list += ','.join(command[27]) + ' [아이디1] [아이디2]...(최대 12명)\n'     #/경주
+			command_list += ','.join(command[35]) + ' [판매금액] (거래소세금)\n'     #/수수료
+			command_list += ','.join(command[36]) + ' [거래소금액] [실거래금액] (거래소세금)\n'     #/페이백
+			command_list += ','.join(command[13]) + ' [아이디]\n'     #/정산
+			command_list += ','.join(command[14]) + ' 또는 ' + ','.join(command[14]) + ' 0000, 00:00\n'     #/보스일괄
+			command_list += ','.join(command[15]) + '\n'     #/q
+			command_list += ','.join(command[16]) + ' [할말]\n'     #/v
+			command_list += ','.join(command[17]) + '\n'     #/리젠
+			command_list += ','.join(command[18]) + '\n'     #/현재시간
+			command_list += ','.join(command[24]) + '\n'     #/킬초기화
+			command_list += ','.join(command[25]) + '\n'     #/킬횟수 확인
+			command_list += ','.join(command[25]) + ' [아이디]\n'     #/킬
+			command_list += ','.join(command[26]) + ' [아이디]\n'     #/킬삭제
+			command_list += ','.join(command[33]) + ' [아이디] 또는 ' + ','.join(command[33]) + ' [아이디] [횟수]\n'     #/킬차감
+			command_list += ','.join(command[29]) + '\n'     #/아이템 목록 초기화
+			command_list += ','.join(command[30]) + '\n'     #/아이템 목록 확인
+			command_list += ','.join(command[30]) + ' [아이템] 또는 ' + ','.join(command[30]) + ' [아이템] [개수]\n'     #/아이템 목록 입력
+			command_list += ','.join(command[31]) + ' [아이템]\n'     #/아이템 목록에서 삭제
+			command_list += ','.join(command[32]) + ' [아이템] 또는 ' + ','.join(command[32]) + ' [아이템] [개수]\n'     #/아이템 차감
+			command_list += ','.join(command[19]) + '\n'     #/공지
+			command_list += ','.join(command[19]) + ' [공지내용]\n'     #/공지
+			command_list += ','.join(command[20]) + '\n'     #/공지삭제
+			command_list += ','.join(command[21]) + ' [할말]\n'     #/상태
+			command_list += ','.join(command[28]) + ' 사다리, 정산, 척살, 경주, 아이템\n'     #/채널설정
 			command_list += ','.join(command[34]) + ' ※ 관리자만 실행 가능\n\n'     #서버나가기
-			command_list += ','.join(command[22]) + '\n'     #보스탐
-			command_list += ','.join(command[23]) + '\n'     #!보스탐
+			command_list += ','.join(command[22]) + '\n'     #/보스
+			command_list += ','.join(command[23]) + '\n'     #/보스탐
 			command_list += '[보스명]컷 또는 [보스명]컷 0000, 00:00\n'  
 			command_list += '[보스명] 컷 또는 [보스명] 컷 0000, 00:00\n'   
 			command_list += '[보스명]멍 또는 [보스명]멍 0000, 00:00\n'     
@@ -1684,7 +1684,7 @@ class mainCog(commands.Cog):
 		else:
 			return
 
-	################ 사다리 결과 출력 ################ 
+	################ 뽑기 결과 출력 ################ 
 	@commands.command(name=command[12][0], aliases=command[12][1:])
 	async def ladder_(self, ctx : commands.Context, *, args : str = None):
 		if ctx.message.channel.id == basicSetting[7] or ctx.message.channel.id == basicSetting[8]:
